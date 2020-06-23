7157bfea893de4bbca8ec0ddb291a6a07689862a
jayrajzala <jayrajzala6@gmail.com>
2020-06-24 01:54:23 +0530

    Optimizing navbar for mobile layouyt

--- js/native/main.js
+++ js/native/main.js
@@ -7,11 +7,22 @@ const selectElement = function (element) {
 };
 
 // burger menu click to open
-selectElement('.menu-icons').addEventListener('click', function () {
+selectElement('.menu-icons').addEventListener('click', function (e) {
   selectElement('.nav__right').classList.toggle('current');
   selectElement('body').classList.toggle('noscroll');
+  //if clicked on burger, run function that attached event listners.
+  if (e.target.classList.contains('nav__right__burger-menu')) {
+    return getTarget(e);
+  }
 });
 
-selectElement('a').addEventListener('click', function (e) {
-  e.preventDefault();
-});
+function getTarget(e) {
+  // run only if EventListener is not already present
+  if (!e.target.getAttribute('listener')) {
+    selectElement('.nav__right.current .nav__right__menu').addEventListener('click', function (el) {
+      el.target.parentElement.classList.toggle('clicked');
+    });
+  }
+  //adding attribute that stores info about whether eventListener is present or not.
+  e.target.setAttribute('listener', 'true');
+}
