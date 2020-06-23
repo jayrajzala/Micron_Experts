859d7e910212d2f04e7f7c37df6ceac4b8e9b884
jayrajzala <jayrajzala6@gmail.com>
2020-06-23 18:38:01 +0530

    making nav bar responsive

--- js/native/main.js
+++ js/native/main.js
@@ -1,2 +1,17 @@
 //navbar sticky
 stickybits('nav', {useStickyClasses: true});
+
+// general function to select elements
+const selectElement = function (element) {
+  return document.querySelector(element);
+};
+
+// burger menu click to open
+selectElement('.menu-icons').addEventListener('click', function () {
+  selectElement('.nav__right').classList.toggle('current');
+  selectElement('body').classList.toggle('noscroll');
+});
+
+selectElement('a').addEventListener('click', function (e) {
+  e.preventDefault();
+});
